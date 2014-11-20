64047f88da7d3d62c051c43d29503632b7ac2246
Rory Douglas <rory1douglas@gmail.com>
2014-11-20 00:02:17 -0500

    Added shebang to run.sh, added db/mq host links and API token vars to fig

--- run.sh
+++ run.sh
@@ -1,2 +1,6 @@
+#!/bin/bash
+
+cd twitter-slurper
+
 sbt "run-main com.rory.twitterslurper.PersistMain" &&
-sbt "run-main com.rory.twitterslurper.SlurpMain" &&
+sbt "run-main com.rory.twitterslurper.SlurpMain"
